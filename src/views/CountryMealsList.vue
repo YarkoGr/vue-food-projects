e40055779a0d7926435d873d26c14c44abcfdbd3
Yaroslav Pasko <yaroslavpasko81@gmail.com>
2021-01-19 12:21:34 +0200

    :construction:

--- src/views/CountryMealsList.vue
+++ src/views/CountryMealsList.vue
@@ -43,34 +43,4 @@ export default {
 </script>
 
 <style lang="scss">
-.countryMealsList {
-  text-align: center;
-  &__title {
-    @include text($h60, 700, $main-color);
-    font-family: $font-cookie;
-  }
-  &__list {
-    @include flex(center, $wrap: wrap);
-  }
-  &__list-item {
-    width: calc(100% / 24 * 4);
-    text-align: center;
-    @include flex(start, center, column);
-    padding: 0 15px 15px 15px;
-    &:hover > span {
-      border: 1px solid $main-color;
-    }
-    &:hover {
-      cursor: pointer;
-    }
-  }
-
-  &__list-item-title {
-    @include text($h32, 400, $main-color);
-    font-family: $font-cookie;
-    border: 1px solid transparent;
-    width: 100%;
-    height: 100%;
-  }
-}
 </style>
